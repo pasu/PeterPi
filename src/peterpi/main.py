2d8d2d09c88033bbcb7b3b18e5f76a4045e9ea38
Peter Lu <bjfubjfu@gmail.com>
2025-11-06 13:50:34 +0100

    t10

--- src/peterpi/main.py
+++ src/peterpi/main.py
@@ -25,46 +25,6 @@ def add(a: int, b: int) -> int:
         raise ValueError(msg)
     return a + b
 
-def foo1(a):
-    c = a + 1  # noqa: W291
-    if a > 10:
-        return 10
-    return a + c
-
-def foo2(a):
-  b = 0  # noqa: F841
-  c = a + 1  # noqa: W291
-  if a > 0:  # noqa: SIM102
-    if a > 10:
-      return 10
-  return a + c
-
-def foo3(a):  # noqa: RET503
-    b = 0  # noqa: F841
-    c = a + 1  # noqa: W291
-    if a > 0:
-        if a > 10:
-            return 10
-        return a + c
-
-def foo4():
-    menu_1 = {
-        "1": "Start Game",
-        "2": "Load Game",
-        "3": "Options",
-        "4": "Exit"
-        }
-
-    # fmt: off
-    menu_2 = {
-        "1": "Start Game",
-        "2": "Load Game",
-        "3": "Options",
-        "4": "Exit"
-        }
-    # fmt: on
-    return menu_1, menu_2
-
 def main() -> None:
     print(f"Running peterpi version {__version__}")
     print(f"Result is: 1+2 = {add(1, 2)}")
